81f67f082ba1c14ade775f0294f22f3c7ca10134
Kien Hoang <kienhoangdev@gmail.com>
2015-04-19 16:52:06 -0400

    Added progress bars to homepage.

--- app/src/main/java/com/example/kienhoang/habitapp/HomeFragment.java
+++ app/src/main/java/com/example/kienhoang/habitapp/HomeFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Date;
+
 /**
  * Created by kienhoang on 4/17/15.
  */
@@ -14,6 +16,11 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Adjust the progress bars.
+        DatabaseHandler databaseHandler = new DatabaseHandler(getActivity());
+        DailyData todayData = databaseHandler.getDailyDataByDate(new Date());
+
 
         return rootView;
     }
