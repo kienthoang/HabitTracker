68b374ea868a71280f2b04abf29c70e11a87bb01
Kien Hoang <kienhoangdev@gmail.com>
2015-04-21 21:33:43 -0400

    Done. Ready for demo.

--- app/src/main/java/com/example/kienhoang/habitapp/HabitGraphActivity.java
+++ app/src/main/java/com/example/kienhoang/habitapp/HabitGraphActivity.java
@@ -84,7 +84,7 @@ public class HabitGraphActivity extends Activity {
 
             graph.getViewport().setYAxisBoundsManual(true);
             graph.getViewport().setMinY(0);
-            graph.getViewport().setMaxY(5);
+            graph.getViewport().setMaxY(4);
         }
 
         databaseHandler.close();
